04251c7a41bbfcc70ce015e4ebbd1f562b173f10
Nagasha Christine <cnagasha@usiu.ac.ke>
2025-04-01 17:03:00 +0300

    My first commit

---
+++ hello world1.py
@@ -0,0 +1,16 @@
+
+'''
+print('welcome')
+username=input('Enter your name')
+userage=input('Enter your age')
+print('My name is Nagasha Christine ')
+print('I am 66 years old ')
+print('yo yo am kiddinghdhd')
+'''
+             A        B
+            ______   ______
+          /        \ /        \
+         /           X           \
+        |           |             |
+         \________/ \________/
+
